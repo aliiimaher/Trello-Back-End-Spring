6ecc589061684765c7c996e19427b8a7c172c8d8
Mohsen Zahmatkesh <msn.zmt81@gmail.com>
2022-07-09 21:28:42 +0430

    create checklist method fix

--- src/main/java/project/trello/service/ChecklistService.java
+++ src/main/java/project/trello/service/ChecklistService.java
@@ -2,7 +2,9 @@ package project.trello.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import project.trello.model.Card;
 import project.trello.model.Checklist;
+import project.trello.repository.CardRepository;
 import project.trello.repository.ChecklistRepository;
 
 import java.util.List;
@@ -12,10 +14,12 @@ public class ChecklistService {
 
 
     private final ChecklistRepository checklistRepository;
+    private final CardRepository cardRepository;
 
     @Autowired
-    public ChecklistService(ChecklistRepository checklistRepository) {
+    public ChecklistService(ChecklistRepository checklistRepository, CardRepository cardRepository) {
         this.checklistRepository = checklistRepository;
+        this.cardRepository = cardRepository;
     }
 
     public List<Checklist> getChecklists() {
@@ -23,7 +27,11 @@ public class ChecklistService {
     }
 
 
-    public Checklist createChecklist(Checklist checklist) {
-        return checklistRepository.save(checklist);
+    public Card createChecklist(Long card_id,Checklist checklist) {
+        checklistRepository.save(checklist);
+        Card card = cardRepository.findById(card_id).get();
+        Checklist checklist1 = checklistRepository.findById(checklist.getId()).get();
+        card.getChecklists().add(checklist1);
+        return cardRepository.save(card);
     }
 }
